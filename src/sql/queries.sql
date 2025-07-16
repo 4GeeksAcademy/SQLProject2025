686c92d87e421c76e089ba7e576692033f7b0bef
Mohadia <110264060+Mohadia@users.noreply.github.com>
2025-07-16 15:55:50 +0000

    sql completed

--- src/sql/queries.sql
+++ src/sql/queries.sql
@@ -22,31 +22,71 @@ SELECT * FROM observations;
 
 
 -- MISSION 1
--- Your query here;
+SELECT * FROM observations
+LIMIT 10 ;
 
 -- MISSION 2
--- Your query here;
+
+ SELECT DISTINCT region_id FROM observations ;
 
 
 -- MISSION 3
--- Your query here;
+SELECT COUNT(DISTINCT species_id) FROM observations ;
 
 
 -- MISSION 4
--- Your query here;
+SELECT COUNT(*) FROM observations
+WHERE region_id = 2;
 
 
 -- MISSION 5
--- Your query here;
+SELECT COUNT(*) FROM observations
+WHERE observation_date = '1998-08-08';
 
 
 -- MISSION 6
--- Your query here;
+SELECT region_id, COUNT(*) AS count
+FROM observations
+GROUP BY region_id;
 
 
 -- MISSION 7
--- Your query here;
+SELECT species_id, COUNT(*) AS count
+FROM observations
+GROUP BY species_id
+ORDER BY count DESC
+LIMIT 5;
 
 
 -- MISSION 8
--- Your query here;
+SELECT species_id, COUNT(*) AS count
+FROM observations
+GROUP BY species_id
+HAVING COUNT(*) < 5;
+
+
+-- MISSION 9
+SELECT observer, COUNT(*) AS count
+FROM observations
+GROUP BY observer
+ORDER BY count DESC
+LIMIT 5;
+
+-- MISSION 10
+SELECT observations.*, regions.name AS region_name
+FROM observations
+JOIN regions ON observations.region_id = regions.id;
+
+-- MISSION 11
+SELECT observations.*, species.scientific_name
+FROM observations
+JOIN species ON observations.species_id = species.id;
+
+-- MISSION 12
+SELECT regions.name AS region, species.scientific_name, COUNT(*) AS total
+FROM observations
+JOIN species ON observations.species_id = species.id
+JOIN regions ON observations.region_id = regions.id
+GROUP BY region, species.scientific_name
+ORDER BY region, total DESC;
+
